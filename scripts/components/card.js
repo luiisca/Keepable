9b14c59bbcdb4e06e9d02b1cdf641262ef324ec8
luis <luiscaf3r@gmail.com>
2022-02-19 18:56:14 -0500

    Add note-card component

--- scripts/components/card.js
+++ scripts/components/card.js
@@ -1,24 +1,31 @@
 const Card = (function () {
   const template = `
   <div class="note-card">
-    <img src="" alt="">
-
-    <div class="note-card__content">
-      <h2 class="note-card__title">Title</h2>
-      <p class="note-card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cupiditate distinctio dolorum molestiae consectetur, at delectus aliquam adipisci sequi quaerat, itaque dolore quae tempore sint aliquid, harum illum praesentium officiis.</p>
+    <div class="note-card__img-cont">
+      <img src="images/tmp-back.jpg" alt="">
     </div>
 
-    <div class="note-card__footer">
-      <button class="material-icons-outlined">
-        palette
+    <div class="note-card__content">
+      <h2 class="note-card__title font-title font-title--medium mb-1">Title</h2>
+
+      <button class="button button--floating-top">
+        <span class="material-icons-outlined">push_pin</span>
       </button>
 
-      <button class="material-icons-outlined">
-        image
+      <p class="note-card__text font-body font-body--medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cupiditate distinctio dolorum molestiae consectetur, at delectus aliquam adipisci sequi quaerat, itaque dolore quae tempore sint aliquid, harum illum praesentium officiis.</p>
+    </div>
+
+    <div class="note-card__buttons">
+      <button class="button">
+        <span class="material-icons-outlined">palette</span>
       </button>
 
-      <button class="material-icons-outlined">
-        delete
+      <button class="button">
+        <span class="material-icons-outlined">image</span>
+      </button>
+
+      <button class="button">
+        <span class="material-icons-outlined">delete</span>
       </button>
     </div>
   </div>
