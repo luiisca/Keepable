a67f555a3158ed9680ebdc1bc357c06c57878c98
luis <luiscaf3r@gmail.com>
2022-02-20 00:16:16 -0500

    Style header component

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,15 +1,22 @@
 const Header = (function () {
   const template = `
-  <section class="section">
-    <header>
-    <span class="material-icons-outlined"> menu </span>
-    <h1 class="heading">Keepable</h1>
-    <div class="search-bar">
-    <span class="material-icons-outlined"> search </span>
-    <input type="text" placeholder="Search" />
+  <header class="header flex mb-2">
+    <button class="header__menu-button button">
+      <span class="header__menu-icon material-icons-outlined"> menu </span>
+    </button>
+
+    <div class="logo-container flex">
+      <img src="images/logo.png" alt="logo" class="logo">
+      <h1 class="font-display font-display--small">Keepable</h1>
     </div>
-    </header>
-  </section>
+
+    <form class="header__search-bar flex">
+      <button class="header__search-button button">
+        <span class="header__search-icon material-icons-outlined"> search </span>
+      </button>
+      <input class="header__input font-title font-title--medium" type="text" placeholder="Search" />
+    </form>
+  </header>
   `;
 
   return {
